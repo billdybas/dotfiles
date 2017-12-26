d1c0a3371d00cffbd6e7f8695d75546dc66e8da0
Bill Dybas <wmdybas@gmail.com>
2017-12-26 02:42:45 -0500

    Add Some Atom Packages & Themes

---
+++ install/atom.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Install Packages
+echo "Installing Atom Packages"
+apm install atom-beautify
+apm install atom-clock
+apm install atom-ternjs
+apm install atom-typescript
+apm install autocomplete-modules
+apm install color-picker
+apm install docblockr
+apm install editorconfig
+apm install emmet
+apm install figlet
+apm install file-icons
+apm install hey-pane
+apm install language-apache
+apm install language-babel
+apm install language-blade
+apm install language-docker
+apm install language-ejs
+apm install language-elixir
+apm install language-elm
+apm install language-graphql
+apm install language-latex
+apm install language-nginx
+apm install language-scala
+apm install latex
+apm install linter
+apm install linter-eslint
+apm install linter-js-standard
+apm install linter-ui-default
+apm install minimap
+apm install minimap-cursorline
+apm install minimap-find-and-replace
+apm install minimap-git-diff
+apm install minimap-linter
+apm install minimap-pigments
+apm install pigments
+apm install prettier-atom
+apm install pretty-json
+apm install toggle-quotes
+apm install tree-view-copy-relative-path
+
+# Install Syntax and UI Themes
+echo "Installing Atom Themes"
+apm install 2-dark-syntax
+apm install an-old-hope-syntax
+apm install atom-material-syntax
+apm install atom-material-ui
+apm install atom-monokai
+apm install cobalt2-syntax
+apm install dark-flat-ui
+apm install duotone-dark-syntax
+apm install duotone-light-syntax
+apm install one-dark-vivid-syntax
+apm install paraiso-dark
+apm install seti-syntax
+apm install seti-ui
+apm install viriato
