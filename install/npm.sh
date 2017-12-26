25ca29a396c6489951ae2809281964a57bae5f0f
Bill Dybas <wmdybas@gmail.com>
2017-12-26 02:44:25 -0500

    Add Some Global NPM Packages

---
+++ install/npm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Packages
+echo "Installing Global NPM Packages"
+npm i -g babel-cli
+npm i -g cloc
+npm i -g create-react-app
+npm i -g eslint
+npm i -g npm-check-updates
+npm i -g prettier
+npm i -g serve
+npm i -g typescript
