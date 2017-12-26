b3744d49d425ded00c323b29f447e8afd6a5f07c
Bill Dybas <wmdybas@gmail.com>
2017-12-26 03:00:09 -0500

    Add Notes About Manual Installations

---
+++ install/manual.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "These Applications Must be Installed Manually:"
+echo "----------------------------------------------"
+echo "Docker - https://www.docker.com/docker-mac"
+echo "----------------------------------------------"
+echo "Postgres.app - https://postgresapp.com/"
+echo "----------------------------------------------"
+echo "Postico - https://eggerapps.at/postico/"
+echo "----------------------------------------------"
