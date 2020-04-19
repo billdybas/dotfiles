c98bef7e0d844478e17182abb38597efe53f59a8
Bill Dybas <wmdybas@gmail.com>
2020-04-19 14:43:49 -0700

    Add More Homebrew Formulae

--- install/brew.sh
+++ install/brew.sh
@@ -9,6 +9,7 @@ brew upgrade
 # Install Homebrew Formulae
 # Command Line Tools
 brew install bash
+brew install bash-completion
 brew install git
 brew install tmux
 brew install tree
@@ -16,10 +17,21 @@ brew install vim
 brew install watchman
 brew install wget
 brew install zsh
+brew install aws-iam-authenticator
+brew install bat
+brew install circleci
+brew install cloc
+brew install diff-so-fancy
+brew install htop
+brew install hub
+brew install jq
+brew install kubernetes-cli
+brew install terraform
 # Languages + Package Managers
 brew install elixir
 brew install haskell-stack
 brew install node
+brew install nvm
 brew install rbenv
 brew install sbt
 brew install scala
@@ -29,15 +41,21 @@ brew install graphicsmagick
 brew install imagemagick --with-webp
 # Databases
 brew install mongodb
+brew install postgresql
+brew install redis
 brew install rethinkdb
 brew install sqlite
+brew tap sqitchers/sqitch
+brew install sqitch --with-postgres-support
 
 # Install Homebrew Cask Formulae
 brew cask install discord
 brew cask install mactex
 brew cask install spotify
 brew cask install spotify-notifications
+brew cask install spotmenu
 brew cask install xquartz
+brew cask install licecap
 # Browsers
 brew cask install firefox
 brew cask install google-chrome
@@ -52,14 +70,22 @@ brew cask install postman # See: https://www.getpostman.com
 brew cask install sublime-text # See: https://www.sublimetext.com
 echo -e "Remember to install Package Control: https://packagecontrol.io/installation"
 brew cask install visual-studio-code # See: https://code.visualstudio.com
+brew cask install android-studio
+brew cask install graphiql
+brew cask install graphql-playground
+brwe cask install zeplin
 # Database Related
 brew cask install db-browser-for-sqlite # See: http://sqlitebrowser.org
 brew cask install mysqlworkbench # See: https://www.mysql.com/products/workbench
 brew cask install sequel-pro # See: https://sequelpro.com
+brew cask install postgres
 # Systems Related
 # Install Virtualbox before Vagrant since Vagrant depends on it
 brew cask install virtualbox # See: https://www.virtualbox.org
 brew cask install vagrant # See: https://www.vagrantup.com
+brew cask install docker
+brew cask install google-cloud-sdk
+brew cask install ngrok
 
 # Install Homebrew Cask Versions Formulae
 brew tap caskroom/versions
